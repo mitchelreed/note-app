ead9c179fccb989255f3abc7e8271fe451748702
mitch <jmitchelreed@gmail.com>
2021-03-07 23:45:49 -0500

    added styles and tried to append body text to text area

--- src/ListOfNotes.js
+++ src/ListOfNotes.js
@@ -1,6 +1,7 @@
 import firebase from './firebase';
 import { RiDeleteBack2Line } from "react-icons/ri";
 import { FaRegStickyNote } from "react-icons/fa";
+import Note from './Note.js'
 
 
 
@@ -11,8 +12,28 @@ function ListOfNotes({ notesArray }) {
 		const dbRef = firebase.database().ref()
 
 		dbRef.child(uniqueKey).remove()
+		document.getElementById('bodyText').textContent = ''
 	}
-	
+
+	// trying to append the body of the NOTE to the TEXTAREA
+	const showNote = (noteData) => {
+		// click on h3 and grab the unique key value from firebase
+		console.log(noteData.noteContent.bodyInput)
+		
+		// query select the text area and change its inner text content to be equal to the body text connected to the h3 clicked
+		// document.getElementById('bodyText').textContent = noteData.noteContent.bodyInput
+		return (
+
+			<Note passToNote={noteData} />
+		)
+	}
+
+	// const showNote = (noteData) => {
+	// 	console.log(noteData)
+	// 	return (
+	// 	)
+	// }
+
 	return (
 		<>
 
@@ -22,19 +43,17 @@ function ListOfNotes({ notesArray }) {
 			<div className="listContainer">
 				<ul className="listOfNotes">
 					{
-						
+
 						notesArray.map((noteData) => {
 
-								// ensures that an empty string does not display
-							if(noteData.noteContent.titleInput !== "" && noteData.noteContent.bodyInput !== ""){
-
-
+							// ensures that an empty string does not display
+							if (noteData.noteContent.titleInput !== "" && noteData.noteContent.bodyInput !== "") {
 
 								return (
-	
+
 									<div key={noteData.uniqueKey}>
 										<li>
-											<h3>
+											<h3  onClick={() => showNote(noteData)} >
 												{noteData.noteContent.titleInput}
 											</h3>
 											<div className="paragraphAndDeleteContainer">
@@ -45,14 +64,13 @@ function ListOfNotes({ notesArray }) {
 											</div>
 										</li>
 									</div>
-								
+
 								)
 							}
 
-							
-						})
+						}).reverse() // <--- displays the array in reverse order so the newest note is shown first
 					}
-				
+
 				</ul>
 			</div>
 		</>
--- src/Note.js
+++ src/Note.js
@@ -2,71 +2,71 @@ import firebase from './firebase';
 import { useEffect, useState } from 'react';
 import ListOfNotes from './ListOfNotes';
 
-function Note() {
-
+function Note({ passToNote }) {
 	const [notesArray, setNotesArray] = useState([])
 	const [titleInput, setTitleInput] = useState('')
 	const [bodyInput, setBodyInput] = useState('')
 	const dbRef = firebase.database().ref()
-
+	
+	
+	
 	useEffect(() => {
-
+		
 		dbRef.on('value', (data) => {
-
+			
 			const noteData = data.val()
-
-
+			
 			const notePad = []
-
+			
 			for (let noteKey in noteData) {
 				notePad.push({
 					uniqueKey: noteKey,
 					noteContent: noteData[noteKey]
 				})
 			}
-
+			
 			setNotesArray(notePad)
-
+			
 		})
-
+		
 	}, [])
-
-
+	
+	
 	const handleChange = (event, input) => {
 		
-
-			input(event.target.value)
+		input(event.target.value)
 		
 	}
-
+	
 	const handleClick = (event) => {
 		event.preventDefault()
-
+		
 		const dbRef = firebase.database().ref()
-
+		
+	
 		// ensures that an empty string does not get pushed to firebase
 		if (titleInput !== "" && bodyInput !== "") {
-		dbRef.push({ titleInput, bodyInput })
+			dbRef.push({ titleInput, bodyInput })
 		}
+		
+		// clears the state holding whatever was captured by the handle change function above
 		setTitleInput("")
 		setBodyInput("")
-
+		
+		// clears the actual input and text area after a note is saved
 		document.querySelector(".bodyText").value = ''
 		document.querySelector(".noteTitle").value = ''
-
-
+		
 	}
-
-
+	
 	return (
-
 		<>
 			<form>
 				<label htmlFor="noteTitle" className="sr-only"></label>
 				<input className="noteTitle" type="text" id="noteTitle" placeholder="Note title" onChange={(event) => handleChange(event, setTitleInput)} />
 				<label htmlFor="bodyText" className="sr-only"></label>
 				<textarea className="bodyText" id="bodyText" placeholder="Start typing..." onChange={(event) => handleChange(event, setBodyInput)}>
-
+				
 				</textarea>
 				<button className="save button" onClick={handleClick}>Save</button>
 			</form>
@@ -75,4 +75,4 @@ function Note() {
 	)
 }
 
-export default Note 
+export default Note
